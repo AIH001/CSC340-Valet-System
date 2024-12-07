436da2836f607f51b87a675bb4c0e1e2a7f03c4e
William Wong <williamw55955@gmail.com>
2024-12-06 23:18:06 -0800

    finished guest and ticket cpp files

--- Guest.cpp
+++ Guest.cpp
@@ -1,5 +1,40 @@
-//
-// Created by Ahmad Harris on 12/5/24.
-//
+// created by william wong
 
 #include "Guest.h"
+#include <iostream>
+
+using namespace std;
+
+// Default constructor
+Guest::Guest() {
+    ticketNum = 0;
+    name = "Guest";
+    isVIP = false;
+}
+
+Guest::Guest(string guestName) {
+    ticketNum = 0;
+    name = guestName;
+    isVIP = false;
+    cout << "Guest created with name: " << name << endl;
+}
+
+Guest::Guest(int ticketNumber, string guestName) {
+    ticketNum = ticketNumber;
+    name = guestName;
+    isVIP = false;
+    cout << "Guest created with ticket number: " << ticketNum 
+         << " and name: " << name << endl;
+}
+
+void Guest::getCar() {
+    cout << name << " is retrieving their car with ticket number: " << ticketNum << endl;
+}
+
+void Guest::getStatus() {
+    cout << name << " is checking the status of their car." << endl;
+}
+
+
+
+
--- Ticket.cpp
+++ Ticket.cpp
@@ -1,9 +1,47 @@
-//
-// Created by Ahmad Harris on 12/5/24.
-//
+// created by william wong
 
 #include "Ticket.h"
+#include <iostream>
+
+using namespace std;
+
+Ticket::Ticket() {
+    ticketNum = 0;
+    price = 0.0;
+    carDetails = Car("", "", "", "");
+    guestDetails = Guest();
+    cout << "Ticket created with default values." << endl;
+}
+
+Ticket::Ticket(int ticketNum) {
+    this->ticketNum = ticketNum;
+    price = 0.0;
+    carDetails = Car("", "", "", "");
+    guestDetails = Guest(ticketNum, "Default Guest");
+    cout << "Ticket created with ticket number: " << this->ticketNum << endl;
+}
 
 int Ticket::getTicketNum() const {
-    return this->ticketNum;
+    return ticketNum;
 }
+
+void Ticket::setCarDetails(std::string make, std::string model, std::string color, std::string licensePlate) {
+    cout << "Setting car details for ticket number: " << ticketNum << endl;
+
+    carDetails.setMake(make);
+    carDetails.setModel(model);
+    carDetails.setColor(color);
+    carDetails.setPlate(licensePlate);
+
+    cout << "Car details set to: " 
+         << carDetails.getMake() << " "
+         << carDetails.getModel() << " (" 
+         << carDetails.getColor() << "), License Plate: " 
+         << carDetails.getLicensePlate() << endl;
+
+    guestDetails = Guest(ticketNum, "John Doe");
+    cout << "Guest details set for ticket number: " << ticketNum << endl;
+}
+
+
+
